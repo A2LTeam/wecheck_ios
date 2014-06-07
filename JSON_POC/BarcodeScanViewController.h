f767c5f9c8fae120d0bcb16cf5823532aa7e1763
Adam Cheung <adamcheung@croque-monsieur.local>
2014-06-07 10:23:25 +0800

    Working Storyboard before breakfast

---
+++ JSON_POC/BarcodeScanViewController.h
@@ -0,0 +1,13 @@
+//
+//  BarcodeScanViewController.h
+//  JSON_POC
+//
+//  Created by Adam Cheung on 7/6/14.
+//  Copyright (c) 2014 Adam Cheung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BarcodeScanViewController : UIViewController
+
+@end
