755db4e0f71cc9324ae463cfff457e1c3208a9cf
Wai Lok Tsang <tsangwailok@wai-lok-tsangs-macbook-pro.local>
2014-06-20 18:53:48 +0800

    Initial Commit

---
+++ wecheckTests/wecheckTests.m
@@ -0,0 +1,34 @@
+//
+//  wecheckTests.m
+//  wecheckTests
+//
+//  Created by Wai Lok Tsang on 6/20/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface wecheckTests : XCTestCase
+
+@end
+
+@implementation wecheckTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
