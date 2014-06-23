0840fc41cedac0e1633335317232ba1a931d89c6
Adam Cheung <adamcheung@croque-monsieur.local>
2014-06-23 23:23:10 +0800

    test commit by Adam

---
+++ JSON_POC/wecheck_ios/wecheck/WCViewController.h
@@ -0,0 +1,13 @@
+//
+//  WCViewController.h
+//  wecheck
+//
+//  Created by Wai Lok Tsang on 6/20/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCViewController : UIViewController
+
+@end
