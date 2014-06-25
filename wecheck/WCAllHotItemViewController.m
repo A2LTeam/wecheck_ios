228c78eff814d3611fb2408e1f1e5fb233ecc001
Wai Lok Tsang <tsangwailok@wai-lok-tsangs-macbook-pro.local>
2014-06-25 23:40:50 +0800

    add AllHotItemViewController

---
+++ wecheck/WCAllHotItemViewController.m
@@ -0,0 +1,49 @@
+//
+//  WCAllHotItemViewController.m
+//  wecheck
+//
+//  Created by Wai Lok Tsang on 6/25/14.
+//  Copyright (c) 2014 Wai Lok Tsang. All rights reserved.
+//
+
+#import "WCAllHotItemViewController.h"
+
+@interface WCAllHotItemViewController ()
+
+@end
+
+@implementation WCAllHotItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
