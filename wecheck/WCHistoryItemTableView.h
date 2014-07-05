1c3cb21f05919fc11156a50f05b6d26773ab7994
Adam Cheung <adamcheung@croque-monsieur.local>
2014-07-05 14:52:27 +0800

    Add back missing files

---
+++ wecheck/WCHistoryItemTableView.h
@@ -0,0 +1,13 @@
+//
+//  WCHistoryItemTableView.h
+//  wecheck
+//
+//  Created by Adam Cheung on 5/7/14.
+//  Copyright (c) 2014 Adam Cheung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCHistoryItemTableView : UITableView
+
+@end
---
+++ wecheck/WCHistoryItemTableView.m
@@ -0,0 +1,31 @@
+//
+//  WCHistoryItemTableView.m
+//  wecheck
+//
+//  Created by Adam Cheung on 5/7/14.
+//  Copyright (c) 2014 Adam Cheung. All rights reserved.
+//
+
+#import "WCHistoryItemTableView.h"
+
+@implementation WCHistoryItemTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
